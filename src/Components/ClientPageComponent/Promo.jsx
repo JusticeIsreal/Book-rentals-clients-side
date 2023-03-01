b6edf02e219213c0e32a3a65d052c73a7ca6a1a2
Justice Isreal <justiceyba@gmail.com>
2023-03-01 02:44:20 +0100

    footer and review section completed

--- src/Components/ClientPageComponent/Promo.jsx
+++ src/Components/ClientPageComponent/Promo.jsx
@@ -7,32 +7,14 @@ function Promo({ promo }) {
         <div className="content">
           <h3>Deal of the day</h3>
           <h1>Up to 40% discount</h1>
-          {/* <p>
-            Get interesting motivational books at discount price of 40% off.
-            Note that discount prices are only available for signed up users.
-            Click to shop now
-          </p> */}
           <a href="#" className="btn">
             Shop now
           </a>
         </div>
-        <div
-          style={{
-            border: "2px solid red",
-          }}
-        >
+        <div className="fade-con">
           <Fade arrows={false} className="fade-con">
             {promo.map((item, index) => (
-              <div
-                className="image"
-                key={index}
-                style={{
-                  // width: "50px",
-                  // height: "100px",
-
-                  position: "absolute",
-                }}
-              >
+              <div className="image" key={index}>
                 <img src={item.productimage} alt="" key={index._id} />
               </div>
             ))}
@@ -45,19 +27,3 @@ function Promo({ promo }) {
 }
 
 export default Promo;
-// <Slide
-// // arrows={true}
-// // transitionDuration={1000}
-// // duration={1000}
-// // autoplay={true}
-// // infinite={true}
-// // canSwipe={true}
-// >
-//   {promo.map((index) => (
-//     <div className="each-fade" key={index._id}>
-//       <div className="image-container">
-//         <img src={index.productimage} alt="" style={{ width: "50px" }} />
-//       </div>
-//     </div>
-//   ))}
-// </Slide>;
--- src/Components/ClientPageComponent/Review.jsx
+++ src/Components/ClientPageComponent/Review.jsx
@@ -2,107 +2,52 @@ import React from "react";
 
 function Review() {
   return (
-    <div>
+    <div style={{ marginTop: "170px" }}>
       <section className="reviews" id="reviews">
         <h1 className="heading">
-          <span>Client's reviews</span>{" "}
+          <span style={{ background: "transparent", border: "none" }}>
+            Reviews
+          </span>{" "}
         </h1>
 
-        <div className="swiper reviews-slider">
-          <div className="swiper-wrapper">
-            <div className="swiper-slide box">
+        <div className="swiper reviews-slider reviews-main-con">
+          <div className="swiper-wrapper reviews-con">
+            <div className="review-box">
               <img src="../../../public/images/pic-1.png" alt="" />
               <h3>John deo</h3>
-              <p>
-                Books from this website are top notch, i commend to be part of
-                distributor someday
-              </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
-              </div>
+              <p>Books from this website are top notch,</p>
             </div>
 
-            <div className="swiper-slide box">
+            <div className="review-box bb">
               <img src="../../../public/images/pic-2.png" alt="" />
               <h3>Clara Jackson</h3>
               <p>
                 So interesting, i would like to buy more books especially the
-                book (Love), it's a great novel, please keep posting more
+                book (Love),
               </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
-              </div>
             </div>
-
-            <div className="swiper-slide box">
+            <div className="review-box">
               <img src="../../../public/images/pic-3.png" alt="" />
               <h3>Mavin Jerry</h3>
-              <p>
-                Very great novels and motivational books,please keep up with the
-                provision of these books and also the novels
-              </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
-              </div>
+              <p>Very great novels and motivational books,</p>
             </div>
-            <div className="swiper-slide box">
-              {/* <img src="../../../public/CSS/images/pic-4.png" alt="" /> */}
+            <div className="review-box">
+              <img src="../../../public/images/pic-4.png" alt="" />
               <h3>Tracy Ben</h3>
-              <p>
-                So inspiring and nice novels i would like to read more, get more
-                motivated and also, be a premium user in this site
-              </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
-              </div>
+              <p>So inspiring and nice novels i would like to read more,</p>
             </div>
-
-            <div className="swiper-slide box">
+            <div className="review-box">
               <img src="../../../public/images/pic-5.png" alt="" />
               <h3>Peter Martins</h3>
               <p>
                 Nice and interesting novels, please can you post some novels
                 with series
               </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
-              </div>
             </div>
-
-            <div className="swiper-slide box">
+            <div className="review-box">
               <img src="../../../public/images/pic-6.png" alt="" />
               <h3>Natasha Richards</h3>
-              <p>
-                These are amazing novels, they so much inspire me. I want to buy
-                A bundle of a novel in series
-              </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
-              </div>
+              <p>These are amazing novels, they so much inspire me.</p>
             </div>
           </div>
         </div>
